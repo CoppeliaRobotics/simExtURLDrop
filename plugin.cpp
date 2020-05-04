1f5e3af77379c7ee32a9aaf57ed33b33afd08eca
Federico Ferri <federico.ferri.it@gmail.com>
2020-05-04 15:46:52 +0200

    use module info api

--- plugin.cpp
+++ plugin.cpp
@@ -23,8 +23,8 @@ public:
         //if(!registerScriptStuff())
         //    throw std::runtime_error("failed to register script stuff");
 
-        simSetModuleInfo(PLUGIN_NAME, 0, "URL Drop Plugin", 0);
-        simSetModuleInfo(PLUGIN_NAME, 1, BUILD_DATE, 0);
+        setExtVersion("URL Drop Plugin");
+        setBuildDate(BUILD_DATE);
     }
 
     void onEnd()
