024a74bf6d6c844ce0239cbff9813eaf449d053d
Federico Ferri <federico.ferri.it@gmail.com>
2023-01-18 15:57:21 +0100

    + openURL()

--- plugin.cpp
+++ plugin.cpp
@@ -9,7 +9,14 @@
 #include "stubs.h"
 
 #include "eventfilter.h"
+#include <QString>
 #include <QWidget>
+#ifdef _WIN32
+#include <QSettings>
+#else
+#include <QUrl>
+#include <QDesktopServices>
+#endif // _WIN32
 
 class Plugin : public sim::Plugin
 {
@@ -37,8 +44,30 @@ public:
         eventFilter->deleteLater();
     }
 
+    void openURL(openURL_in *in, openURL_out *out)
+    {
+#ifdef _WIN32
+        QSettings httpSetting("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\Shell\\Associations\\UrlAssociations\\http\\UserChoice", QSettings::Registry64Format);
+        QString progId(httpSetting.value("ProgId").toString());
+        QSettings openCmd(QStringLiteral("HKEY_CLASSES_ROOT\\%1\\shell\\open\\command").arg(progId), QSettings::Registry64Format);
+        QString url(QString::fromStdString(in->url));
+        QString cmd(openCmd.value("Default").toString().arg(url));
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
+        ::ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(si);
+        ::ZeroMemory(&pi, sizeof(pi));
+        QByteArray cmdba = cmd.toLocal8Bit();
+        ::CreateProcess(NULL, cmdba.data(), NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+#else
+        QUrl url = QUrl(QString::fromStdString(in->url));
+        QDesktopServices::openUrl(url);
+#endif
+    }
+
 private:
     EventFilter *eventFilter;
 };
 
 SIM_PLUGIN(PLUGIN_NAME, PLUGIN_VERSION, Plugin)
+#include "stubsPlusPlus.cpp"
