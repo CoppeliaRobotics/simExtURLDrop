6f9f8c01ea5f418505c11b2acf015c28075eb0d8
Coppelia <mfreese@gmx.ch>
2023-06-29 09:30:03 +0200

    support also local files, including CAD files

--- simAddOnURL drop service.lua
+++ simAddOnURL drop service.lua
@@ -19,27 +19,56 @@ function sysCall_msg(event)
         local impFiles=''
         for _,t in ipairs(ts) do
             t=simURLDrop.rewriteURL(t)
---            if string.startswith(t,'http://') or string.startswith(t,'https://') then
+            if string.startswith(t,'http://') or string.startswith(t,'https://') then
+                local file=simURLDrop.getURLFile(t)
                 if string.endswith(t,'.ttm') or string.endswith(t,'.simmodel.xml') then
-                    local data=simURLDrop.getURL(t)
-                    sim.loadModel(data)
+                    loadModel(file)
                 elseif string.endswith(t,'.ttt') or string.endswith(t,'.simscene.xml') then
-                    local data=simURLDrop.getURL(t)
-                    sim.loadScene(data..'@keepCurrent')
+                    loadScene(file..'@keepCurrent')
                 elseif string.endswith(t,'.obj') or string.endswith(t,'.stl') or string.endswith(t,'.dxf') or string.endswith(t,'.ply') or string.endswith(t,'.dae') then
                     if #impFiles>0 then
                         impFiles=impFiles..';'
                     end
-                    if sim.getInt32Param(sim.intparam_platform)==0 then
-                        impFiles=impFiles..string.gsub(t,"^file:///","")
-                    else
-                        impFiles=impFiles..string.gsub(t,"^file://","")
-                    end
+                    impFiles=impFiles..file
                 end
---            end
+            elseif string.startswith(t,'file://') then
+                if sim.getInt32Param(sim.intparam_platform)==0 then
+                    file=string.gsub(t,"^file:///","")
+                else
+                    file=string.gsub(t,"^file://","")
+                end
+                if string.endswith(t,'.ttm') or string.endswith(t,'.simmodel.xml') then
+                    loadModel(file)
+                elseif string.endswith(t,'.ttt') or string.endswith(t,'.simscene.xml') then
+                    loadScene(file..'@keepCurrent')
+                elseif string.endswith(t,'.obj') or string.endswith(t,'.stl') or string.endswith(t,'.dxf') or string.endswith(t,'.ply') or string.endswith(t,'.dae') then
+                    if #impFiles>0 then
+                        impFiles=impFiles..';'
+                    end
+                    impFiles=impFiles..file
+                end
+            end
         end
         if #impFiles>0 then
-            simAssimp.importShapesDlg(impFiles)
+            pcall(simAssimp.importShapesDlg,impFiles)
         end
+    end
+end
+
+function loadModel(f)
+    local res,ret=pcall(sim.loadModel,f)
+    if res then
+        sim.addLog(sim.verbosity_scriptinfos, "model loaded.")
+    else
+        sim.addLog(sim.verbosity_scripterrors, "failed loading model: "..ret)
+    end
+end
+
+function loadScene(f)
+    local res,ret=pcall(sim.loadScene,f)
+    if res then
+        sim.addLog(sim.verbosity_scriptinfos, "scene loaded.")
+    else
+        sim.addLog(sim.verbosity_scripterrors, "failed loading scene: "..ret)
     end
 end
