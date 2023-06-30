5a35338de8db832ecc8e3ac3fa94d62a34ec2a3d
Coppelia <mfreese@gmx.ch>
2023-06-30 08:27:26 +0200

    pcall on simURLDrop.getURL

--- simAddOnURL drop service.lua
+++ simAddOnURL drop service.lua
@@ -20,16 +20,20 @@ function sysCall_msg(event)
         for _,t in ipairs(ts) do
             t=simURLDrop.rewriteURL(t)
             if string.startswith(t,'http://') or string.startswith(t,'https://') then
-                local file=simURLDrop.getURL(t,simURLDrop.download_mode.file)
-                if string.endswith(t,'.ttm') or string.endswith(t,'.simmodel.xml') then
-                    loadModel(file)
-                elseif string.endswith(t,'.ttt') or string.endswith(t,'.simscene.xml') then
-                    loadScene(file..'@keepCurrent')
-                elseif string.endswith(t,'.obj') or string.endswith(t,'.stl') or string.endswith(t,'.dxf') or string.endswith(t,'.ply') or string.endswith(t,'.dae') then
-                    if #impFiles>0 then
-                        impFiles=impFiles..';'
+                local res,file=pcall(simURLDrop.getURL,t,simURLDrop.download_mode.file)
+                if res then
+                    if string.endswith(t,'.ttm') or string.endswith(t,'.simmodel.xml') then
+                        loadModel(file)
+                    elseif string.endswith(t,'.ttt') or string.endswith(t,'.simscene.xml') then
+                        loadScene(file..'@keepCurrent')
+                    elseif string.endswith(t,'.obj') or string.endswith(t,'.stl') or string.endswith(t,'.dxf') or string.endswith(t,'.ply') or string.endswith(t,'.dae') then
+                        if #impFiles>0 then
+                            impFiles=impFiles..';'
+                        end
+                        impFiles=impFiles..file
                     end
-                    impFiles=impFiles..file
+                else
+                    sim.addLog(sim.verbosity_scripterrors, "failed downloading file: "..t)
                 end
             elseif string.startswith(t,'file://') then
                 if sim.getInt32Param(sim.intparam_platform)==0 then
